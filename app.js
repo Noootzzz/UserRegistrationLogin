c599f107796c07da15514888a487aed18bde7646
Noot <nathan.github22@gmail.com>
2024-07-24 10:35:49 +0200

    render home page

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import mysql from 'mysql'
 import dotenv from 'dotenv'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 dotenv.config({
     path: './.env'
@@ -15,13 +20,19 @@ const db = mysql.createConnection({
     database: process.env.DATABASE //.env value
 })
 
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory))
+
+app.set('view engine', 'hbs')
+
 db.connect((error) => {
     if (error) throw error
     console.log(`DATABASE CONNEXION ===> OK`)
 })
 
 app.get('/', (req, res) => {
-    res.send("<h1>Home</h1>")
+    // res.send("<h1>Home</h1>")
+    res.render("index")
 })
 
 app.listen(process.env.PORT, () => {
